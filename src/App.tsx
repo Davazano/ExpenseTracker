7207ef23d31dfe25a992177a5551eb84f580c1ce
Davazano <davideoguche@gmail.com>
2023-07-05 23:08:01 +0100

    Passed items and delete item handler to ExpenseList Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 
 function App() {
+  const [items, SetItems] = useState([
+    { id: 1, description: "Orange", amount: 5, category: "Groceries" },
+    { id: 2, description: "Apple", amount: 7, category: "Groceries" },
+    { id: 3, description: "Phone", amount: 900, category: "Electronic" },
+  ]);
+
+  const handleDeleteItem = (id: number) => {
+    console.log("Deleting", id);
+    SetItems(items.filter((item) => item.id !== id));
+  };
+
   return (
     <div>
-      <ExpenseList />
+      <ExpenseList items={items} onDelete={handleDeleteItem} />
     </div>
   );
 }
